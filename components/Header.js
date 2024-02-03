776d4215e22255c71ad681e63ce14ab34bd59bb9
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2024-02-03 14:06:02 +0000

    Added Navigation links and adjusted Navbar colour

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,21 @@
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="bg-red-600 text-black p-3">
-      <h1 className="text-xl font-semibold">Land of Smiles</h1>
+    <header className="flex justify-between items-center bg-thailand-red text-white p-3">
+      <h1 className="text-xl font-semibold">
+        <Link href="/">Land of Smiles</Link>
+      </h1>
+      <nav>
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/weather">Weather</Link>
+          </li>
+          <li>
+            <Link href="/planner">Itinerary Planner</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
