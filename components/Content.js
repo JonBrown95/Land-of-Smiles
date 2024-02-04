c8d279b65c77caf7ed4b5ff87af0a50146f01616
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2024-02-04 21:23:51 +0000

    Added button with custom gradient

--- components/Content.js
+++ components/Content.js
@@ -1,18 +1,16 @@
 "use client";
-import { ClerkProvider, useClerk } from "@clerk/clerk-react";
-import { SignIn, SignUp } from "@clerk/clerk-react";
-import { useEffect } from "react";
-import Header from "../components/Header";
 
 function Content() {
-  
-
   return (
-      <div className="min-h-100 flex flex-col items-center justify-center bg-white">
-        <p className="mt-4 text-xl font-semibold">Content</p>
-          
+    <div className="h-screen flex flex-col items-center justify-center bg-gray-100">
+      <div class="relative p-0.5 w-full max-w-max rounded-full border-0 bg-gradient-to-b from-thailand-blue to-thailand-red ">
+        <div class="relative bg-gray-100 text-12-20 sm:text-sm text-thailand-blue px-[18px] py-1.5 sm:px-5 sm:py-2 flex items-center gap-2 rounded-full uppercase tracking-[.20em]">
+          Discover all Thailand has to offer
+        </div>
       </div>
-    
+
+      <p className="mt-4 text-xl font-semibold">Content</p>
+    </div>
   );
 }
 
