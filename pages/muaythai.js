40a3833fef809154442a5be29cf5e8a300e8b3f1
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2024-02-04 20:05:29 +0000

    Added Muay Thai page - contains info on Muay Thai around Thailand

---
+++ pages/muaythai.js
@@ -0,0 +1,134 @@
+function MuayThai() {
+  return (
+    <div className="min-h-screen bg-thailand-blue flex items-center justify-center">
+      <div className="max-w-4xl w-full mx-6 p-8 py-2 bg-white rounded-md shadow-md">
+        <h1 className="text-4xl font-bold mb-6 text-thailand-blue">
+          Explore the World of Muay Thai in Thailand
+        </h1>
+        <p className="text-gray-800 mb-6">
+          Muay Thai, known as "The Art of Eight Limbs," is a cultural treasure
+          and a powerful martial art that originated in Thailand. Beyond its
+          roots, Muay Thai has evolved into a global phenomenon, attracting
+          enthusiasts and fighters from all corners of the world.
+        </p>
+
+        <h2 className="text-2xl font-bold mb-4 text-thailand-blue">
+          Discover Authentic Training Camps
+        </h2>
+        <p className="text-gray-700 mb-6">
+          Thailand boasts a rich tradition of Muay Thai, offering diverse
+          training camps suitable for practitioners of all skill levels. Immerse
+          yourself in the authentic atmosphere of these camps, where tradition
+          meets modern training techniques.
+        </p>
+
+        <h2 className="text-2xl font-bold mb-4 text-thailand-blue">
+          Benefits of Muay Thai Training
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+          <div className="flex items-start">
+            <div className="rounded-full bg-thailand-red p-3 mr-4">
+              <svg
+                className="w-6 h-6 text-white"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 13l4 4L19 7"
+                ></path>
+              </svg>
+            </div>
+            <p className="text-gray-700">
+              <span className="font-bold">Physical Fitness:</span> Improve
+              cardiovascular health and overall fitness through Muay Thai's
+              intense workouts.
+            </p>
+          </div>
+          <div className="flex items-start">
+            <div className="rounded-full bg-thailand-red p-3 mr-4">
+              <svg
+                className="w-6 h-6 text-white"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 13l4 4L19 7"
+                ></path>
+              </svg>
+            </div>
+            <p className="text-gray-700">
+              <span className="font-bold">Mental Discipline:</span> Develop
+              focus, discipline, and mental toughness through Muay Thai
+              training.
+            </p>
+          </div>
+          <div className="flex items-start">
+            <div className="rounded-full bg-thailand-red p-3 mr-4">
+              <svg
+                className="w-6 h-6 text-white"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 13l4 4L19 7"
+                ></path>
+              </svg>
+            </div>
+            <p className="text-gray-700">
+              <span className="font-bold">Self-Defense Skills:</span> Learn
+              effective self-defense techniques as part of Muay Thai's
+              comprehensive training.
+            </p>
+          </div>
+          <div className="flex items-start">
+            <div className="rounded-full bg-thailand-red p-3 mr-4">
+              <svg
+                className="w-6 h-6 text-white"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 13l4 4L19 7"
+                ></path>
+              </svg>
+            </div>
+            <p className="text-gray-700">
+              <span className="font-bold">Stress Relief:</span> Muay Thai serves
+              as an excellent stress reliever, allowing you to channel your
+              energy positively.
+            </p>
+          </div>
+        </div>
+
+        <p className="text-gray-700 mt-6">
+          Experience the unique blend of tradition and modernity as you delve
+          into the world of Muay Thai in Thailand. Whether you're a seasoned
+          fighter or a fitness enthusiast, Muay Thai offers a journey of
+          physical and mental transformation.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default MuayThai;
