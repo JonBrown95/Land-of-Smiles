8fa25fadf97da076ca522bdfe96606a302d5715b
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2024-02-06 19:49:00 +0000

    Adjusted Footer colouring to a light gray

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="relative bg-white text-white">
+    <footer className="relative bg-gray-100 text-white">
       <div className="p-10 text-xs md:text-sm flex flex-col gap-5 justify-center items-center"></div>
 
       <div className="absolute inset-x-0 bottom-0 bg-thailand-red h-8"></div>
@@ -11,3 +11,4 @@ const Footer = () => {
 };
 
 export default Footer;
+
