48d62c3c8b506f3e676d6de720e308a02c93ee6b
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2024-04-03 18:59:02 +0100

    Restyled Muay Thai page and added images

--- pages/muaythai.js
+++ pages/muaythai.js
@@ -1,47 +1,27 @@
+import React from "react";
+
 function MuayThai() {
   return (
-    <div className="min-h-screen bg-thailand-blue flex items-center justify-center">
-      <div className="max-w-4xl w-full mx-6 p-8 py-2 bg-white rounded-md shadow-md">
-        <h1 className="text-4xl font-bold mb-6 text-thailand-blue">
-          Explore the World of Muay Thai in Thailand
+    <section className="min-h-screen bg-[#f0f0f0] py-12 flex items-center justify-center">
+      <article className="max-w-4xl w-full mx-6 p-8">
+        <h1 className="text-3xl md:text-5xl font-bold mb-6 text-thailand-blue text-center">
+          Muay Thai in Thailand
         </h1>
-        <p className="text-gray-800 mb-6">
+        <p className="text-lg md:text-xl text-gray-700 mb-8 leading-relaxed text-center">
           Muay Thai, known as "The Art of Eight Limbs," is a cultural treasure
           and a powerful martial art that originated in Thailand. Beyond its
           roots, Muay Thai has evolved into a global phenomenon, attracting
           enthusiasts and fighters from all corners of the world.
         </p>
 
-        <h2 className="text-2xl font-bold mb-4 text-thailand-blue">
-          Discover Authentic Training Camps
-        </h2>
-        <p className="text-gray-700 mb-6">
-          Thailand boasts a rich tradition of Muay Thai, offering diverse
-          training camps suitable for practitioners of all skill levels. Immerse
-          yourself in the authentic atmosphere of these camps, where tradition
-          meets modern training techniques.
-        </p>
-
-        <h2 className="text-2xl font-bold mb-4 text-thailand-blue">
-          Benefits of Muay Thai Training
-        </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div className="flex items-start">
-            <div className="rounded-full bg-thailand-red p-3 mr-4">
-              <svg
+            <div className="rounded-full flex items-center justify-center w-12 h-12 mr-4">
+              <img
+                src="/tickIcon.svg"
+                alt="Tick icon"
                 className="w-6 h-6 text-white"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M5 13l4 4L19 7"
-                ></path>
-              </svg>
+              />
             </div>
             <p className="text-gray-700">
               <span className="font-bold">Physical Fitness:</span> Improve
@@ -50,21 +30,12 @@ function MuayThai() {
             </p>
           </div>
           <div className="flex items-start">
-            <div className="rounded-full bg-thailand-red p-3 mr-4">
-              <svg
+            <div className="rounded-full  flex items-center justify-center w-12 h-12 mr-4">
+              <img
+                src="/tickIcon.svg"
+                alt="Tick icon"
                 className="w-6 h-6 text-white"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M5 13l4 4L19 7"
-                ></path>
-              </svg>
+              />
             </div>
             <p className="text-gray-700">
               <span className="font-bold">Mental Discipline:</span> Develop
@@ -73,21 +44,12 @@ function MuayThai() {
             </p>
           </div>
           <div className="flex items-start">
-            <div className="rounded-full bg-thailand-red p-3 mr-4">
-              <svg
+            <div className="rounded-full  flex items-center justify-center w-12 h-12 mr-4">
+              <img
+                src="/tickIcon.svg"
+                alt="Tick icon"
                 className="w-6 h-6 text-white"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M5 13l4 4L19 7"
-                ></path>
-              </svg>
+              />
             </div>
             <p className="text-gray-700">
               <span className="font-bold">Self-Defense Skills:</span> Learn
@@ -96,21 +58,12 @@ function MuayThai() {
             </p>
           </div>
           <div className="flex items-start">
-            <div className="rounded-full bg-thailand-red p-3 mr-4">
-              <svg
+            <div className="rounded-full flex items-center justify-center w-12 h-12 mr-4">
+              <img
+                src="/tickIcon.svg"
+                alt="Tick icon"
                 className="w-6 h-6 text-white"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M5 13l4 4L19 7"
-                ></path>
-              </svg>
+              />
             </div>
             <p className="text-gray-700">
               <span className="font-bold">Stress Relief:</span> Muay Thai serves
@@ -120,14 +73,42 @@ function MuayThai() {
           </div>
         </div>
 
-        <p className="text-gray-700 mt-6">
+        <p className="text-lg md:text-xl text-gray-700 mt-8 leading-relaxed">
           Experience the unique blend of tradition and modernity as you delve
           into the world of Muay Thai in Thailand. Whether you're a seasoned
           fighter or a fitness enthusiast, Muay Thai offers a journey of
           physical and mental transformation.
         </p>
-      </div>
-    </div>
+
+        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+          <div className="md:flex">
+            <div className="md:shrink-0">
+              <img
+                className="h-48 w-full object-cover md:h-full md:w-48"
+                src="boxing.jpg"
+                alt="Modern building architecture"
+              />
+            </div>
+            <div className="p-8">
+              <div className="uppercase tracking-wide text-sm text-red-500 font-semibold">
+                Travelling to Thailand for Muay Thai?
+              </div>
+              <a
+                href="https://muaythaiadvisor.com/"
+                className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+              >
+                Check out Muay Thai Advisor
+              </a>
+              <p className="mt-2 text-slate-500">
+                Looking to take your team away on a retreat to enjoy awesome
+                food and take in some sunshine? We have a list of places to do
+                just that.
+              </p>
+            </div>
+          </div>
+        </div>
+      </article>
+    </section>
   );
 }
 
