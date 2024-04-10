7de9aed20c928f65f647eb3bb8871932912ff859
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2024-04-10 21:38:36 +0100

    Added FavIcon and custom font

--- pages/_app.js
+++ pages/_app.js
@@ -9,15 +9,24 @@ import {
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Chatbot from "../components/Chatbot";
+import '@fontsource/krub';
+import Head from "next/head";
+
+
 
 
 
 function MyApp({ Component, pageProps }) {
   return (
+    
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
     >
       <SignedIn>
+      <Head>
+      <title>Land of Smiles</title>
+			<link rel='icon' href='/icon.png' />
+		</Head>
         <Header />
         <Chatbot/>
         <Component {...pageProps} />
