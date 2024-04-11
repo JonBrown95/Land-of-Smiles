266f04e45b0c21af8e54c2051c014af6d4bb0e9c
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2024-04-11 08:21:16 +0100

    Restyled Hero with background image of a Thai beach

--- components/Hero.js
+++ components/Hero.js
@@ -5,35 +5,26 @@ import { useEffect } from "react";
 import Header from "../components/Header";
 
 function Hero() {
-  const { signOut, user } = useClerk();
-
-  const handleSignOut = async () => {
-    await signOut();
-  };
-
-  useEffect(() => {
-    console.log("User Object:", user);
-  }, [user]);
-
   return (
-    <>
-      <div className="min-h-screen flex flex-col items-center justify-center bg-thailand-blue">
-        <p className="mt-4 text-xl font-semibold">
-          {user ? `Welcome, ${user.primaryEmailAddress}` : "Not signed in"}
-        </p>
+    <div
+      className="relative h-screen  flex items-center justify-center text-center bg-cover bg-center"
+      style={{
+        backgroundImage: `url(/bg.jpg)`,
+      }}
+    >
+      <div className="absolute top-0 right-0 bottom-0 left-0 bg-gray-900 opacity-55"></div>
 
-        {user ? (
-          <button
-            onClick={handleSignOut}
-            className="mt-4 px-4 py-2 bg-red-600 text-white rounded-md"
-          >
-            Sign Out
-          </button>
-        ) : (
-          <></>
-        )}
-      </div>
-    </>
+      <main className="px-4 sm:px-6 lg:px-8 z-10">
+        <div className="text-center">
+          <h2 className="text-4xl tracking-tight leading-10 font-medium sm:text-5xl text-white sm:leading-none md:text-6xl">
+            <span className="text-white font-bold">Land of Smiles</span>
+          </h2>
+          <p className="mt-3 text-white sm:mt-5 sm:text-md sm:max-w-xl sm:mx-auto md:mt-5">
+            add more here
+          </p>
+        </div>
+      </main>
+    </div>
   );
 }
 
