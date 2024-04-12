8e54db6b76dcfb2e380aed1688160188a4b4c15d
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2024-04-12 13:17:08 +0100

    Created WhyThailand Section

---
+++ components/WhyThailandSection.js
@@ -0,0 +1,20 @@
+function WhyThailandSection() {
+    return (
+      <div className="min-h-0 bg-[#f0f0f0] flex items-center justify-center py-16">
+        <div className="max-w-5xl w-full mx-6 bg-white rounded-3xl shadow-md">
+          <h2 className="text-sm mt-4 font-extrabold tracking-widest text-center text-gray-600">
+            WHY THAILAND?
+          </h2>
+          <div className="max-w-3xl mx-auto px-6">
+            <h2 className="text-5xl py-4 font-extrabold tracking-wide text-center text-black">
+              Unbeleivable food, beautiful scenery and people!
+              
+            </h2>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default WhyThailandSection;
+  
